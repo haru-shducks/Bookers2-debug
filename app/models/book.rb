3c2049a69b240fa707c73a4493c7e3faafca5329
haruto <138562348+haru-shducks@users.noreply.github.com>
2023-09-08 08:51:44 +0000

    [finish]応用課題8b 週間投稿グラフ

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,19 +1,32 @@
 class Book < ApplicationRecord
+#associate---
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-
+#---
 
   validates :title,presence:true
   validates :body,presence:true,length: { minimum: 1, maximum: 200 }
 
-  #posts count
-  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
-  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  
+  
+
+#posts count & posts count graph---
+  #week
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
 
-  # 検索方法分岐
+  #day
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) } # 2日前
+  scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day) } # 3日前
+  scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day) } # 4日前
+  scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) } # 5日前
+  scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) } # 6日前
+#---
+
+# 検索方法分岐---
   def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
@@ -25,6 +38,7 @@ class Book < ApplicationRecord
       Book.where('title LIKE ?', '%' + content + '%')
     end
   end
+#---
 
   def favorited_by?(user)
      favorites.exists?(user_id: user.id)
