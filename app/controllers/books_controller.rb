3c8185782ef26fb4a363efd4beba3ca1a6663a30
haruto <138562348+haru-shducks@users.noreply.github.com>
2023-09-10 13:27:43 +0000

    [finish]応用課題9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,28 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_comment = BookComment.new
     @user = User.find(@book.user_id)
+    #dm機能
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
+
+    unless ReadCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.read_counts.create(book_id: @book.id)
+    end
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,9 @@
 class Book < ApplicationRecord
-#associate---
+  
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-#---
+  has_many :read_counts, dependent: :destroy
 
   validates :title,presence:true
   validates :body,presence:true,length: { minimum: 1, maximum: 200 }
